71d91da5324cecdec3d8f5d1526a2278940656e1
Wilovy09 <srwilovy09@gmail.com>
2024-08-29 00:25:52 -0600

    Commit realizado el 2024-08-29 00:25:52

--- nixosModules/nvim/lua/base/init.lua
+++ nixosModules/nvim/lua/base/init.lua
@@ -3,11 +3,13 @@ vim.g.mapleader = " "
 vim.wo.relativenumber = true
 vim.wo.number = true
 
-vim.opt.tabstop = 2        -- Número de espacios que representa una tabulación
-vim.opt.shiftwidth = 2     -- Número de espacios utilizados para la indentación
-vim.opt.expandtab = true   -- Convierte tabs a espacios
+vim.opt.tabstop = 2      -- Número de espacios que representa una tabulación
+vim.opt.shiftwidth = 2   -- Número de espacios utilizados para la indentación
+vim.opt.expandtab = true -- Convierte tabs a espacios
 
-vim.api.nvim_set_keymap('n', '<C-s>', ':w!<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Esc>', ':q!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-s>", ":w!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-Esc>", ":q!<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeFindFileToggle<CR>", { noremap = true, silent = true })
 
 require("base.plugins.lazy")
