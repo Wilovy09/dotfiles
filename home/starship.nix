13d2baa25867f8efdb588061f7a9895360be44d7
Wilovy09 <srwilovy09@gmail.com>
2024-10-02 03:00:09 -0600

    :sparkles: Add better prompt in StarshipRS

--- home/starship.nix
+++ home/starship.nix
@@ -4,7 +4,7 @@
     settings = {
       add_newline = false;
       format = ''
-        ($git_branch) ($git_status)($nix_shell)($nodejs)($swift)($golang)($rust)($python)$directory$character
+        (($git_branch) ($nodejs)($rust)($python)($git_status))($nix_shell)$directory$character
       '';
       directory = {
         format = "[󰉋 ](fg:#D8A657 bg:none)[$path ](fg:#D8A657 bg:none)";
@@ -21,46 +21,46 @@
       git_status = {
         format = "[$all_status $ahead_behind]($style)\n";
         style = "fg:#F0F0F0 bg:none";
-        conflicted = " 🔪";
-        ahead = " 🏎️$\{count\}";
-        behind = " 🐢$\{count\}";
-        diverged = " 🏎️$\{ahead_count\}🐢$\{behind_count\}";
-        up_to_date = " 💯";
-        untracked = " 🤷$\{count\}";
-        stashed = " 📦";
-        modified = " 📝$\{count\}";
-        staged = " ✅$\{count\}";
-        renamed = " ✏️$\{count\}";
-        deleted = " 🗑️$\{count\}";
+        conflicted = "🔪$\{count\} ";
+        ahead = "🏎️$\{count\} ";
+        behind = "🐢$\{count\} ";
+        diverged = "🏎️$\{ahead_count\} 🐢$\{behind_count\} ";
+        up_to_date = "💯";
+        untracked = "🤷$\{count\} ";
+        stashed = "📦$\{count\} ";
+        modified = "📝$\{count\} ";
+        staged = "✅$\{count\} ";
+        renamed = "🚚$\{count\} ";
+        deleted = "🗑️$\{count\} ";
       };
       character = {
         success_symbol = "[󱐋](bold yellow)";
         error_symbol = "[󱐋](bold red)";
       };
+      rust = {
+        symbol = "🦀";
+        style = "fg:#f65308";
+        format = "[$symbol$version ]($style)";
+      };
       nodejs = {
         symbol = "🐢";
         style = "fg:#83cd29";
-        format = "[$symbol]($style)";
+        format = "[$symbol$version ]($style)";
+      };
+      python = {
+        symbol = "🐍";
+        style = "fg:#FF0";
+        format = "[$symbol$version ]($style)";
       };
       golang = {
         symbol = "🦦";
         style = "fg:#78d5fd";
-        format = "[$symbol]($style)";
-      };
-      rust = {
-        symbol = "🦀";
-        style = "fg:#f65308";
-        format = "[$symbol]($style)";
-      };
-      python = {
-        symbol = "🐍";
-        style = "fg:#fee262";
-        format = "[$symbol]($style)";
+        format = "[$symbol$version ]($style)";
       };
       swift = {
         symbol = "🦒";
-        style = "fg:#FF0";
-        format = "[$symbol]($style)";
+        style = "fg:#F4993A";
+        format = "[$symbol$version ]($style)";
       };
     };
   };
