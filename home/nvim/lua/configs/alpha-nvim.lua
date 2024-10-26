fc57a182034b8f4843dd7325bff4f041d92f860e
Wilovy09 <srwilovy09@gmail.com>
2024-10-25 19:10:41 -0600

    :art: Se cambia banner de NVIM

--- home/nvim/lua/configs/alpha-nvim.lua
+++ home/nvim/lua/configs/alpha-nvim.lua
@@ -1,50 +1,56 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
 
-		dashboard.section.header.val = {
-			[[                                                                      ]],
-			[[       ████ ██████           █████      ██                      ]],
-			[[      ███████████             █████                              ]],
-			[[      █████████ ███████████████████ ███   ███████████    ]],
-			[[     █████████  ███    █████████████ █████ ██████████████    ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████    ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████   ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████  ]],
-		}
-
+    dashboard.section.header.val = {
+      [[                  █ ██ ██ ]],
+      [[               █ ████████ █          █ ]],
+      [[  █ ██       █ █████████████ █      ██ ]],
+      [[ ▇█ ███    █ █████████████████ █   ███ █ ]],
+      [[ ███ ██   ███████████████████████  █████ ]],
+      [[  █████  ████████████████████████  ████ ]],
+      [[   ███ ████████████████████████████ ██ ]],
+      [[     █  ███████████  ██ █ ███████████ ]],
+      [[      █████████████  ██ █ ██████████ ]],
+      [[       ███████████   ██   ██████████ ]],
+      [[      ██░░██████████████████████░░░██ ]],
+      [[      ██ ░  ░░███████  ██████░░  ░ ██ ]],
+      [[       ██ ▒     ▒▒▒▒▒▒▒▒▒▒▒     ▒ ██ ]],
+      [[        ██                        █ ]],
+      [[         █                       █ ]],
+    }
     -- stylua: ignore
     dashboard.config.layout = {
       { type = "padding", val = 1 }, dashboard.section.header,
-      { type = "padding", val = 3 }, dashboard.section.buttons,
-      { type = "padding", val = 3 }, dashboard.section.footer,
+      { type = "padding", val = 1 }, dashboard.section.buttons,
+      { type = "padding", val = 1 }, dashboard.section.footer,
     }
-		dashboard.section.buttons.opts.spacing = 0
+    dashboard.section.buttons.opts.spacing = 0
 
     -- stylua: ignore
     dashboard.section.buttons.val = {
-      dashboard.button("e",       "📝 New File",  ":ene <BAR> startinsert<CR>"),
+      dashboard.button("e", "📝 New File", ":ene <BAR> startinsert<CR>"),
       dashboard.button("SPC f f", "🔍 Find File", ":Telescope find_files<CR>"),
       dashboard.button("SPC f g", "🇼 Find Word", ":Telescope live_grep<CR>"),
-      dashboard.button(" CTRL h", "🌳 NVIMTree",  ":NvimTreeFindFileToggle<CR>"),
+      dashboard.button(" CTRL h", "🌳 NVIMTree", ":NvimTreeFindFileToggle<CR>"),
     }
 
-		local function footer()
-			local total_plugins = require("lazy").stats().count
-			local datetime = os.date(" %d-%m-%Y   %H:%M")
-			local version = vim.version()
-			local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+    local function footer()
+      local total_plugins = require("lazy").stats().count
+      local datetime = os.date(" %d-%m-%Y   %H:%M")
+      local version = vim.version()
+      local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
 
-			return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
-		end
-		dashboard.section.footer.val = footer()
+      return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
+    end
+    dashboard.section.footer.val = footer()
 
-		alpha.setup(dashboard.opts)
-	end,
+    alpha.setup(dashboard.opts)
+  end,
 }
